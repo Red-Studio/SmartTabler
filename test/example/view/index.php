a5312eaaa531cbc602aedda355ea1a85a0395eb0
dmitryk <dmitry.kmita@intechsystems.lv>
2014-09-19 22:11:37 +0300

    Expanded functionality, created an example with css and other fancy stuff.

---
+++ test/example/view/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="view/css/style.css">
+        <title>Test</title>
+    </head>
+    <body>
+        <div class="table-block">
+            <?=$tableView;?>
+        </div>
+    </body>
+</html>
